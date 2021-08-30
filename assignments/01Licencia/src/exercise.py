7531cc26bc4bd46e099481ecb364503e9798449b
MonserratA <A00832871@itesm.mx>
2021-08-29 20:01:53 -0500

    292021

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,6 +1,17 @@
 
 def main():
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    if edad >= 18: 
+        identi= input ("¿Tienes identificación oficial?  (s/n): ")
+        if identi == "s":
+            print("Trámite de licencia concedido")
+        elif identi == "n":
+            print("No cumples requisitos")
+        else:
+            print("Respuesta incorrecta")
+
+    else:
+        print("No cumples requisitos")
 
 
 if __name__ == '__main__':
