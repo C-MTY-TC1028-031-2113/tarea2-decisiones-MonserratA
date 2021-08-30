471196906558d8af5a4e40830c0c4f00385fc6d5
MonserratA <A00832871@itesm.mx>
2021-08-29 20:47:29 -0500

    292021

--- assignments/03BMI/src/exercise.py
+++ assignments/03BMI/src/exercise.py
@@ -1,6 +1,21 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    p= float(input("Peso en kg: "))
+    a= float(input("Altura en m: "))
+    if p<=0 or a<=0:
+        print ("Revisa tus datos, alguno de ellos es erróneo.")
+    else:
+        imc = p / a**2
+        if imc <20 :
+            print ("PESO BAJO")
+        elif imc>=20 and imc<25:
+            print ("NORMAL")
+        elif imc >=25 and imc <30:
+            print ("SOBREPESO")
+        elif imc >=30 and imc <40:
+            print ("OBESIDAD")
+        elif imc >=40 :
+            print ("OBESIDAD MORBIDA")
 
 if __name__=='__main__':
     main()
